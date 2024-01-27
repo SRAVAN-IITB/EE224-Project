ab9e097ab63dad1c226f7e357e4f23f1f0c8ecd5
Sravan K Suresh <132607113+SRAVAN-IITB@users.noreply.github.com>
2024-01-27 15:09:26 +0530

    These were the changes made from Chinmay's room with the presence of Singhvi just before the board worked for the first time!

--- Codes/Memory.vhdl
+++ Codes/Memory.vhdl
@@ -4,10 +4,10 @@ use IEEE.NUMERIC_STD.all;
 
 entity Memory is
 	port(Address:in STD_LOGIC_VECTOR(15 downto 0);
-	     Input:in STD_LOGIC_VECTOR(15 downto 0);
+	     Input:in STD_LOGIC_VECTOR(7 downto 0);
 		  data_write: in STD_LOGIC_VECTOR(15 downto 0);
 		  data_out: out STD_LOGIC_VECTOR(15 downto 0);
-		  Output:out STD_LOGIC_VECTOR(15 downto 0);
+		  Output:out STD_LOGIC_VECTOR(7 downto 0);
 		  clock, MeM_R,MeM_W: in STD_LOGIC
 		  );
 end entity Memory;
@@ -16,17 +16,12 @@ architecture BHV of Memory is
 
 	type array_of_vectors is ARRAY (255 downto 0) of STD_LOGIC_VECTOR(15 downto 0);
 
-signal memory_storage: array_of_vectors := (1 => "1010001000001010",
-                                            2 => "0001111110000001",
-                                            3 => "0001010010000001",
-                                            4 => "0000011010011000",
-														  5 => "1100010001000001",
-                                            6 => "1111101110000000",
-														  7 => "1101101000000001",
-                                            8 => "0000000011100000",
-														  9 => "0001011100000100",
-                                            10 => Input(15 downto 0),	
-														  others => "0000000000000000");
+signal memory_storage: array_of_vectors := (17 => "1001001000011110",
+                                            18 => "1010010001000000",
+														  19 => "0001010010000010",
+														  20 => "1011010001010000",
+													 others => "0000000000000000");
+signal output1: std_logic_vector(15 downto 0);
 															
 	begin
 	
@@ -34,6 +29,7 @@ signal memory_storage: array_of_vectors := (1 => "1010001000001010",
 	memory_write: PROCESS(clock, MeM_W, data_write, Address)
 		begin
 			if(clock' event and clock = '1') then
+			memory_storage(30) <= "00000000" & Input(7 downto 0);
 				if (MeM_W = '1') then
 					memory_storage(to_integer(unsigned(Address))) <= data_write(15 downto 0);
 				else 
@@ -48,7 +44,7 @@ signal memory_storage: array_of_vectors := (1 => "1010001000001010",
 	memory_read: PROCESS(MeM_R, Address, memory_storage)
 	begin
 		if (MeM_R = '1') then
-			if (unsigned(Address) < 4096) then
+			if (unsigned(Address) < 255) then
 					data_out(15 downto 0) <= memory_storage(to_integer(unsigned(Address)));
 			else
 				data_out <= (others => '0');  -- Default assignment for invalid address
@@ -62,6 +58,7 @@ signal memory_storage: array_of_vectors := (1 => "1010001000001010",
 	-- and to_integer then converts that unsigned integer to a standard integer. 
 	-- This resulting integer is used as an index to access the memory_storage array.
 	
-					Output(15 downto 0) <= memory_storage(200);
+					Output1(15 downto 0) <= memory_storage(46);
+					Output(7 downto 0) <= Output1(7 downto 0);
 	
 end architecture BHV;
